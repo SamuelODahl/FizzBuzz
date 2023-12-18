aaaa235d19698a3319f69f750f05782db4d8746c
SamuelODahl <147284190+SamuelODahl@users.noreply.github.com>
2023-12-18 15:54:10 +0100

    Add files via upload

---
+++ FIzzBuzz/FIzzBuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = 0;
+
+for (number = 1; number < 101; number++)
+{
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (number % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (number % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+
+    else
+    {
+        Console.WriteLine(number);
+    }
+
+}
